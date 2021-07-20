ba02cbe18394346989133abc637c80cbeefd9a51
Daniel O <danielolvera2188@gmail.com>
2021-07-20 16:19:55 -0600

    added database call for candidates

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to datbase
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Brave2114!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello World'
-    });
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
-
 //Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
